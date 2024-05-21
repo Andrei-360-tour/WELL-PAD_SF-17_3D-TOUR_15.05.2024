7c31b892b0d8f2d4fa293887a08bcef47f4ca4a0
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-05-21 14:34:49 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,591 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sf-17_overview-1",
+      "name": "SF-17_Overview 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -2.65603770830384,
+        "pitch": 0.5668800647985428,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.877308246556396,
+          "pitch": 0.21868917519531372,
+          "rotation": 0,
+          "target": "1-sf-17_overview-2"
+        },
+        {
+          "yaw": -3.0121678548353312,
+          "pitch": 0.4306751883128861,
+          "rotation": 0,
+          "target": "2-sf-17_wellhead"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sf-17_overview-2",
+      "name": "SF-17_Overview 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.16186307846276193,
+        "pitch": 0.6231386918088795,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2041157028715492,
+          "pitch": -0.012454314951813927,
+          "rotation": 0,
+          "target": "0-sf-17_overview-1"
+        },
+        {
+          "yaw": -0.5443565387331617,
+          "pitch": 0.6827122010096822,
+          "rotation": 0,
+          "target": "2-sf-17_wellhead"
+        },
+        {
+          "yaw": 1.461982895537541,
+          "pitch": 0.0353120123828905,
+          "rotation": 0,
+          "target": "5-sf-17_security-building"
+        },
+        {
+          "yaw": -2.3231695445824503,
+          "pitch": 0.2354274493297126,
+          "rotation": 0,
+          "target": "12-sf-17_well-pad-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sf-17_wellhead",
+      "name": "SF-17_WellHead",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.35237540004727563,
+        "pitch": 0.31024221529398943,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.39578255133123896,
+          "pitch": -0.23566216966687925,
+          "rotation": 0,
+          "target": "1-sf-17_overview-2"
+        },
+        {
+          "yaw": 0.014248443556622803,
+          "pitch": 0.06562896563148435,
+          "rotation": 0,
+          "target": "3-sf-17_sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 0.7118173468099567,
+          "pitch": 0.42483454494808015,
+          "rotation": 0,
+          "target": "4-sf-17_pipeline"
+        },
+        {
+          "yaw": 2.7756876565640756,
+          "pitch": 0.10204092321149005,
+          "rotation": 0,
+          "target": "12-sf-17_well-pad-5"
+        },
+        {
+          "yaw": 1.4284769201347691,
+          "pitch": -0.2723483179675217,
+          "rotation": 0,
+          "target": "0-sf-17_overview-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sf-17_sucker-rod-pumping-system",
+      "name": "SF-17_Sucker rod pumping system",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.10368857678984433,
+        "pitch": 0.1794311319797206,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5893981143843057,
+          "pitch": 0.12557889515992748,
+          "rotation": 0,
+          "target": "2-sf-17_wellhead"
+        },
+        {
+          "yaw": 2.585331649735057,
+          "pitch": 0.051029241780117474,
+          "rotation": 0,
+          "target": "8-sf-17_well-pad-1"
+        },
+        {
+          "yaw": -2.184222298064487,
+          "pitch": 0.03583167991903835,
+          "rotation": 0,
+          "target": "5-sf-17_security-building"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sf-17_pipeline",
+      "name": "SF-17_Pipeline",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.9896783764666353,
+        "pitch": 0.6103196778269577,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.475875976036148,
+          "pitch": 0.6725897048652971,
+          "rotation": 0,
+          "target": "2-sf-17_wellhead"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sf-17_security-building",
+      "name": "SF-17_Security building",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.23535746379224065,
+        "pitch": 0.08830437827534965,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14774476221548483,
+          "pitch": 0.040542943455040614,
+          "rotation": 0,
+          "target": "1-sf-17_overview-2"
+        },
+        {
+          "yaw": 1.2361615421276024,
+          "pitch": 0.07117839975860818,
+          "rotation": 0,
+          "target": "6-sf-17_waste-pit"
+        },
+        {
+          "yaw": -0.524235734580186,
+          "pitch": 0.05599400748584671,
+          "rotation": 0,
+          "target": "12-sf-17_well-pad-5"
+        },
+        {
+          "yaw": 0.04893985614405949,
+          "pitch": 0.06348473166891111,
+          "rotation": 0,
+          "target": "3-sf-17_sucker-rod-pumping-system"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sf-17_waste-pit",
+      "name": "SF-17_Waste pit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.34647425835156653,
+        "pitch": 0.3113998355002998,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.570436486460773,
+          "pitch": 0.17097872986947849,
+          "rotation": 0,
+          "target": "5-sf-17_security-building"
+        },
+        {
+          "yaw": 0.4524079305102031,
+          "pitch": 0.07721941630911466,
+          "rotation": 0,
+          "target": "7-sf-17_well-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-sf-17_well-pad",
+      "name": "SF-17_Well Pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.4012695943122253,
+        "pitch": 0.2373121422957709,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3132784129859107,
+          "pitch": 0.19959937310816755,
+          "rotation": 0,
+          "target": "6-sf-17_waste-pit"
+        },
+        {
+          "yaw": 0.3432908712295415,
+          "pitch": 0.11160037005624446,
+          "rotation": 0,
+          "target": "8-sf-17_well-pad-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-sf-17_well-pad-1",
+      "name": "SF-17_Well Pad 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.37853446903005405,
+        "pitch": 0.2963507728181263,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.865950809807469,
+          "pitch": 0.20915404188072984,
+          "rotation": 0,
+          "target": "7-sf-17_well-pad"
+        },
+        {
+          "yaw": -0.7629586579643028,
+          "pitch": 0.15455176709734886,
+          "rotation": 0,
+          "target": "3-sf-17_sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 0.6754465929059066,
+          "pitch": 0.14972745790888276,
+          "rotation": 0,
+          "target": "9-sf-17_well-pad-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-sf-17_well-pad-2",
+      "name": "SF-17_Well Pad 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.40885662445678506,
+        "pitch": 0.07640293361717276,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.169047351846329,
+          "pitch": 0.24514905760477212,
+          "rotation": 0,
+          "target": "8-sf-17_well-pad-1"
+        },
+        {
+          "yaw": -0.0954710265375791,
+          "pitch": 0.1066362585391154,
+          "rotation": 0,
+          "target": "10-sf-17_well-pad-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-sf-17_well-pad-3",
+      "name": "SF-17_Well Pad 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -1.0149760326720987,
+        "pitch": 0.030098125364339978,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9528847897750943,
+          "pitch": 0.19765149577684937,
+          "rotation": 0,
+          "target": "9-sf-17_well-pad-2"
+        },
+        {
+          "yaw": 0.21224249419560692,
+          "pitch": 0.10049525285557692,
+          "rotation": 0,
+          "target": "11-sf-17_well-pad-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-sf-17_well-pad-4",
+      "name": "SF-17_Well Pad 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -1.9548424249947338,
+        "pitch": 0.06945721237923763,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.826661160790639,
+          "pitch": 0.08892045365607615,
+          "rotation": 0,
+          "target": "10-sf-17_well-pad-3"
+        },
+        {
+          "yaw": -0.9280313053384539,
+          "pitch": 0.05462512791367935,
+          "rotation": 0,
+          "target": "12-sf-17_well-pad-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-sf-17_well-pad-5",
+      "name": "SF-17_Well Pad 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -1.2300336288723877,
+        "pitch": -0.01041858185688227,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.696702009629149,
+          "pitch": 0.14988455999249872,
+          "rotation": 0,
+          "target": "11-sf-17_well-pad-4"
+        },
+        {
+          "yaw": -1.6554942016730685,
+          "pitch": 0.13779687012781316,
+          "rotation": 0,
+          "target": "2-sf-17_wellhead"
+        },
+        {
+          "yaw": -1.1161759434409078,
+          "pitch": -0.0034235426308732997,
+          "rotation": 0,
+          "target": "5-sf-17_security-building"
+        },
+        {
+          "yaw": -1.6392586067616985,
+          "pitch": -0.09500182732102935,
+          "rotation": 0,
+          "target": "1-sf-17_overview-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WELL PAD SF-17",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
